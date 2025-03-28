fd3be768918b4b017cc29c070cddde31edd1091f
abhishek972986 <abhishekdabas2005@gmail.com>
2025-03-28 13:44:02 +0530

    newews

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,22 +1,24 @@
-
 import { FaCommentDots } from "react-icons/fa";
 
-const Cards = ({card}) => {
+interface CardProps {
+  card: string;
+}
+
+const Cards: React.FC<CardProps> = ({ card }) => {
   return (
     <div className="card-container w-[400px] h-[220px] border border-gray-300 shadow-lg rounded-2xl bg-white flex items-center p-4 transition-all duration-300 hover:shadow-2xl group relative cursor-pointer">
-        <FaCommentDots className='absolute z-2 -right-4 top-[20%] text-red-700 text-3xl'/>
-      {/* Image Section */}
-      <div className="w-1/2 flex justify-center ">
+      <FaCommentDots className="absolute z-2 -right-4 top-[20%] text-red-700 text-3xl" />
+
+      <div className="w-1/2 flex justify-center">
         <div className="w-full h-full flex justify-center relative cursor-pointer">
           <img 
             src={card} 
             alt="Profile" 
-            className="w-[80%] h-full object-cover rounded-2xl transition-transform duration-300  "
+            className="w-[80%] h-full object-cover rounded-2xl transition-transform duration-300"
           />
         </div>
       </div>
 
-      {/* Text Section */}
       <div className="w-1/2 flex flex-col items-center justify-around space-y-2 text-center px-3">
         <p className="text-gray-600 text-sm leading-relaxed">
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
